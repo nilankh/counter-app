2973412f41ab90c73cf8f581d2d5d0179a3b2936
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-07-12 16:22:46 +0530

    rendering list

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,6 +6,7 @@ class counter extends Component {
 
     this.state = {
       count: 0,
+      tags: ['tag1', 'tag2', 'tag3'],
     }
   }
 
@@ -14,6 +15,11 @@ class counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className='btn btn-secondary btn-sm'>Increment</button>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </div>
     )
   }
