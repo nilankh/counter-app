ae60295d436388154d7386b1e5b94c285b67162e
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-07-12 16:01:53 +0530

    Setting attributes done

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -8,13 +8,11 @@ class counter extends Component {
       count: 0,
     }
   }
- 
+
   render() {
     return (
       <div>
-        <span style={{fontSize:30}} className='badge badge-primary m-2'>
-          {this.state.count}
-        </span>
+        <span className='badge badge-primary m-2'>{this.state.count}</span>
         <button className='btn btn-secondary btn-sm'>Increment</button>
       </div>
     )
