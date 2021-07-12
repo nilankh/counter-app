c4c881c55c7d1c4e8e80f568cd6c5d65cd59aa25
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-07-12 16:00:37 +0530

    Setting attributes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -8,12 +8,18 @@ class counter extends Component {
       count: 0,
     }
   }
+  styles = {
+    fontSize: 200,
+    fontWeight: 'bold',
+  }
 
   render() {
     return (
       <div>
-        <span>{this.state.count}</span>
-        <button>Increment</button>
+        <span style={this.styles} className='badge badge-primary m-2'>
+          {this.state.count}
+        </span>
+        <button className='btn btn-secondary btn-sm'>Increment</button>
       </div>
     )
   }
