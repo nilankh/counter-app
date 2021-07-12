3227bd359b441ab381b5c111a3495d205dd668f8
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-07-12 16:33:57 +0530

    Handling events

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,30 +6,34 @@ class counter extends Component {
 
     this.state = {
       count: 0,
-      tags: [],
     }
   }
-
-  //   One technique
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no Tags!</p>
-
-    return (
-      <ul>
-        {this.state.tags.map((tag) => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    )
+  handleIncrement() {
+    console.log('Increment Clicked')
   }
 
   render() {
     return (
       <div>
-        {this.state.tags.length === 0 && 'please create a new tag!'}
-        {this.renderTags()}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className='btn btn-secondary btn-sm'
+        >
+          Increment
+        </button>
       </div>
     )
+  }
+  getBadgeClasses() {
+    let classes = 'badge m-2 badge-'
+    classes += this.state.count === 0 ? 'warning' : 'primary'
+    return classes
+  }
+
+  formatCount() {
+    const { count } = this.state
+    return count === 0 ? 'Zero' : count
   }
 }
 
