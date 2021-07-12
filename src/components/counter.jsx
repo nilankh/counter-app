31a9e87d2b58bd82fd556c9cacf48a876039c147
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-07-12 16:01:29 +0530

    Setting attributes inline css

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -8,15 +8,11 @@ class counter extends Component {
       count: 0,
     }
   }
-  styles = {
-    fontSize: 200,
-    fontWeight: 'bold',
-  }
-
+ 
   render() {
     return (
       <div>
-        <span style={this.styles} className='badge badge-primary m-2'>
+        <span style={{fontSize:30}} className='badge badge-primary m-2'>
           {this.state.count}
         </span>
         <button className='btn btn-secondary btn-sm'>Increment</button>
