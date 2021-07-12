f707adcb459f24e61d990eed6d6068ce1cf5b207
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-07-12 16:26:16 +0530

    condition rendering step1

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -10,28 +10,24 @@ class counter extends Component {
     }
   }
 
+renderTags(){
+    if(this.state.tags.length === 0) return <p>There are no Tags!</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    )
+}
+
   render() {
     return (
       <div>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className='btn btn-secondary btn-sm'>Increment</button>
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        {this.renderTags()}
       </div>
     )
-  }
-  getBadgeClasses() {
-    let classes = 'badge m-2 badge-'
-    classes += this.state.count === 0 ? 'warning' : 'primary'
-    return classes
-  }
-
-  formatCount() {
-    const { count } = this.state
-    return count === 0 ? 'Zero' : count
   }
 }
 
